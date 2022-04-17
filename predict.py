01c3c3ff10c6839501c210df640b3a6fa2b85b07
Alan Cruz <cruz.alan@gmail.com>
2022-04-17 17:21:05 -0400

    converting functions from notebook to cli files

--- predict.py
+++ predict.py
@@ -18,6 +18,8 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
+import os
+
 import pickle
 
 from PIL import Image
@@ -35,17 +37,19 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('input',
                     action='store',
-                    default="error.png",
-                    dest='input',
-                    required=True,
-                    help='Path to image')
+                    default="flowers/valid/1/image_06739.jpg",
+                    help='Path to image, default should be <flowers/valid/1/image_06739.jpg>')
 
 parser.add_argument('checkpoint',
                     action='store',
-                    default="checkpoint.pth",
-                    dest='checkpoint',
-                    required=True,
-                    help='checkpoint file')
+                    default="save_dir/checkpoint.pth",
+                    help='checkpoint file, default should be <save_dir/checkpoint.pth>')
+
+parser.add_argument('--topk', 
+                    action='store',
+                    default=5,
+                    dest="gpu",
+                    help='Use GPU for inference')
 
 parser.add_argument('--category_names',
                     action='store',
@@ -56,12 +60,129 @@ parser.add_argument('--category_names',
 parser.add_argument('--gpu', 
                     action='store',
                     default=False,
-                    dest="gpu"
+                    dest="gpu",
                     help='Use GPU for inference')
 
 cli_args = parser.parse_args()
 
 print('input          = {!r}'.format(cli_args.input))
 print('checkpoint     = {!r}'.format(cli_args.checkpoint))
+print('top k          = {!r}'.format(cli_args.topk))
 print('category_names = {!r}'.format(cli_args.category_names))
 print('gpu            = {!r}'.format(cli_args.gpu))
+
+# TODO: Write a function that loads a checkpoint and rebuilds the model
+def load_checkpoint(filepath):
+
+    path = 'checkpoint.pth'
+    
+    state = torch.load(path)
+    
+    print(config_dictionary)
+
+    model = config_dictionary["model"]
+    optimizer.state_dict = config_dictionary["optimizer_state"]
+
+##### Commented Out to keep workspace size down #####
+load_checkpoint(cli_args.checkpoint)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+
+print(model)
+print(optimizer)
+
+def process_image(image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+
+    # TODO: Process a PIL image for use in a PyTorch model
+    im = image
+    p_im = transform(im)
+    np_im = np.array(p_im)
+
+    # PyTorch expects the color channel to be the first dimension 
+    # but it's the third dimension in the PIL image and Numpy array
+    np_im.transpose((2, 0, 1))
+    
+    return p_im, np_im
+
+
+def imshow(image, ax=None, title=None):
+    """Imshow for Tensor."""
+    if ax is None:
+        fig, ax = plt.subplots()
+    
+    # PyTorch tensors assume the color channel is the first dimension
+    # but matplotlib assumes is the third dimension
+    image = image.numpy().transpose((1, 2, 0))
+    
+    # Undo preprocessing
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = std * image + mean
+    
+    # Image needs to be clipped between 0 and 1 or it looks like noise when displayed
+    image = np.clip(image, 0, 1)
+    
+    ax.imshow(image)
+    
+    return ax
+
+
+def predict(image_path, model, topk=cli_args.topk):
+    ''' Predict the class (or classes) of an image using a trained deep learning model.
+    '''
+
+    # Evaluation Mode
+    model.eval()
+    model = model.to(device)
+    
+    # TODO: Implement the code to predict the class from an image file
+    im = Image.open(image_path)
+    p_im, np_im = process_image(im)
+    
+    # Get torch
+    tnp_im = torch.from_numpy(np_im)
+
+    # https://discuss.pytorch.org/t/expected-stride-to-be-a-single-integer-value-or-a-list/17612/4
+    tnp_im = tnp_im.unsqueeze(0)
+    
+    tnp_im = tnp_im.to(device) 
+    
+    output = model.forward(tnp_im) 
+
+    ps = torch.exp(output)
+
+    probs, classes = ps.topk(topk)
+    
+    return probs, classes
+
+
+im = Image.open(cli_args.input)
+    
+plt.imshow(im)
+plt.show()
+    
+probs, classes = predict(img_path, model)
+
+np_class = classes.cpu().detach().numpy()
+classes = np_class.tolist()[0]
+
+np_probs = probs.cpu().detach().numpy()
+probs = np_probs.tolist()[0]
+    
+np_names = np.array([])
+
+y = []
+    
+for x in range (0, cli_args.topk):
+    y = int(np_class[0,x])
+    name = [ categories_to_name.get(str(y)) ]
+    np_names = np.append(np_names, name)
+
+print("Top k = "+cli_args.topk)
+
+for x in range (0, cli_args.topk)
+    print(np_names[x]+": "+probs[x])
--- train.py
+++ train.py
@@ -4,9 +4,9 @@
 # Prints out training loss, validation loss, and validation accuracy as the network trains
 # Options:
 # Set directory to save checkpoints: python train.py data_dir --save_dir save_directory
-# Choose architecture: python train.py data_dir --arch "vgg13"
-# Set hyperparameters: python train.py data_dir --learning_rate 0.01 --hidden_units 512 --epochs 20
-# Use GPU for training: python train.py data_dir --gpu
+# Choose architecture: python train.py data_dir --arch "vgg16"
+# Set hyperparameters: python train.py data_dir --learning_rate 0.01 --hidden_units 512 --epochs20
+# Use  GPU for training: python train.py data_dir --gpu
 
 import argparse
 
@@ -19,6 +19,8 @@ import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 
 import numpy as np
+
+import os
 
 import pickle
 
@@ -37,26 +39,24 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('data_dir',
                     action='store',
-                    default="data_dir",
-                    dest='data_dir',
-                    required=True,
-                    help='Data directory')
+                    default="flowers",
+                    help='Data directory, default should be <flowers>')
 
 parser.add_argument('--save_dir',
                     action='store',
                     default="~/opt",
                     dest='save_dir',
-                    help='Set directory to save checkpoints')
+                    help='Set directory to save checkpoints, default should be <~/opt>')
 
 parser.add_argument('--arch', 
                     action='store',
-                    default="vgg13",
+                    default="vgg16",
                     dest="arch",
                     help='Choose architecture')
 
 parser.add_argument('--learning_rate', 
                     action='store',
-                    default=0.01,
+                    default=0.001,
                     dest='learning_rate',
                     help='hyperparameter')
 
@@ -74,9 +74,215 @@ parser.add_argument('--epochs',
 
 cli_args = parser.parse_args()
 
-print('data_dir      = {!r}'.format(results.data_dir))
-print('save_dir      = {!r}'.format(results.save_dir))
-print('arch          = {!r}'.format(results.arch))
-print('learning_rate = {!r}'.format(results.learning_rate))
-print('hidden_units  = {!r}'.format(results.hidden_units))
-print('epochs        = {!r}'.format(results.epochs))
+print('data_dir      = {!r}'.format(cli_args.data_dir))
+print('save_dir      = {!r}'.format(cli_args.save_dir))
+print('arch          = {!r}'.format(cli_args.arch))
+print('learning_rate = {!r}'.format(cli_args.learning_rate))
+print('hidden_units  = {!r}'.format(cli_args.hidden_units))
+print('epochs        = {!r}'.format(cli_args.epochs))
+
+data_dir = 'flowers'
+train_dir = data_dir + '/train'
+valid_dir = data_dir + '/valid'
+test_dir = data_dir + '/test'
+
+# TODO: Define your transforms for the training, validation, and testing sets
+transform = transforms.Compose(
+    [
+        transforms.Resize(224),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            [0.485, 0.456, 0.406],                            
+            [0.229, 0.224, 0.225]
+        )
+    ]
+)
+
+# TODO: Load the datasets with ImageFolder
+train_datasets = datasets.ImageFolder(
+    train_dir, 
+    transform=transform
+)
+
+valid_datasets = datasets.ImageFolder(
+    valid_dir, 
+    transform=transform
+)
+
+test_datasets = datasets.ImageFolder(
+    test_dir, 
+    transform=transform
+)
+
+# TODO: Using the image datasets and the trainforms, define the dataloaders
+train_dataloader = torch.utils.data.DataLoader(
+    train_datasets, 
+    batch_size=32,
+    shuffle=True
+)
+
+valid_dataloader = torch.utils.data.DataLoader(
+    valid_datasets, 
+    batch_size=32, 
+    shuffle=True
+)
+
+test_dataloader = torch.utils.data.DataLoader(
+    test_datasets, 
+    batch_size=32, 
+    shuffle=True
+)
+
+with open('cat_to_name.json', 'r') as f:
+    categories_to_name = json.load(f)
+    num_of_categories = len(categories_to_name)
+    print("Number of Categories: ", num_of_categories)
+    print(json.dumps(categories_to_name, sort_keys=True, indent=4))
+
+# load a pre trained model
+# vgg variants classifiers have 25088
+# as input_features of 1st FC layer
+if cli_args.arch == "resnet18":
+    model = models.resnet18(pretrained=True)
+elif cli_args.arch == "alexnet":
+    model = models.alexnet(pretrained=True)
+else:
+    model = models.vgg16(pretrained=True)
+    
+print(model)
+
+img_input = 25088
+recommended_hidden = cli_args.hidden_units # 1024
+output_size=102
+
+# TODO: Build and train your network
+
+# freeze pre-trained parameters
+for param in model.parameters():
+    param.requires_grad = False
+
+# create custom classifier
+recommended_classifier = nn.Sequential(
+    OrderedDict(
+        [
+            # in_feature in fc1 should match in_features
+            # from the first FC layer of a pre-trained classifier
+            ('fc1', nn.Linear(img_input, recommended_hidden)),
+            ('relu', nn.ReLU()),
+            ('dropout', nn.Dropout()),
+
+            # in_features of fc2 should
+            # match out_feature of fc1, and so on.
+            # 
+            # out_features on the last layer should
+            # match the classes (102) you to want to predict
+            ('fc2', nn.Linear(recommended_hidden, output_size)),
+            ('output', nn.LogSoftmax(dim=1))
+        ]
+    )
+)
+
+model.classifier = recommended_classifier
+
+print(model)
+
+# Use GPU if it's available
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+
+criterion = nn.NLLLoss()
+
+# Only train the classifier parameters, feature parameters are frozen
+optimizer = optim.Adam(model.classifier.parameters(), lr=cli_args.learning_rate)
+
+steps = 0
+tot_train_loss = 0
+print_every = 5
+
+train_losses, test_losses = [], []
+
+model.train()
+
+for epoch in range(cli_args.epochs):
+    
+    for inputs, labels in train_dataloader:
+        
+        steps += 1
+        
+        # Move input and label tensors to the default device
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        
+        optimizer.zero_grad()
+
+        # Forward
+        log_ps = model.forward(inputs)
+        loss = criterion(log_ps, labels)
+
+        # Backward
+        loss.backward()
+        optimizer.step()
+
+        tot_train_loss += loss.item()
+
+        train_loss = tot_train_loss / len(train_dataloader.dataset)
+        train_losses.append(train_loss)
+
+        if steps % print_every == 0:
+            print(f"Epoch {epoch+1}/{cli_args.epochs}.. "
+                  f"Train loss: {tot_train_loss/print_every:.3f}.. ")
+            tot_train_loss = 0.0
+
+print('Finished Training')
+
+# TODO: Do validation on the test set
+steps = 0
+tot_test_loss = 0
+test_correct = 0  # Number of correct predictions on the test set
+
+# Turn off gradients for validation, saves memory and computations
+with torch.no_grad():
+    for images, labels in test_dataloader:
+
+        steps += 1
+
+        # Move input and label tensors to the default device
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        images = images.to(device)
+                
+        log_ps = model(images)
+        loss = criterion(log_ps, labels)
+        tot_test_loss += loss.item()
+
+        ps = torch.exp(log_ps)
+        top_p, top_class = ps.topk(1, dim=1)
+        equals = top_class == labels.view(*top_class.shape)
+        test_correct += equals.sum().item()
+
+        # Get mean loss to enable comparison between train and test sets
+        # train_loss = tot_train_loss / len(train_dataloader.dataset)
+        test_loss = tot_test_loss / len(test_dataloader.dataset)
+
+        # At completion of epoch
+        test_losses.append(test_loss)
+
+        print("Epoch: {}/{}.. ".format(1, cli_args.epochs),
+                "Test Loss: {:.3f}.. ".format(test_loss),
+                "Test Accuracy: {:.3f}".format(test_correct / len(test_dataloader.dataset)))
+            
+print("Done Testing")
+
+# TODO: Save the checkpoint 
+model.class_to_idx = train_datasets.class_to_idx
+
+config_dictionary = {
+  'model': model,
+  'optimizer_state': optimizer.state_dict,
+  'epochs': cli_args.epochs
+}
+
+os.mkdir(cli_args.save_dir)
+
+torch.save(config_dictionary, cli_args.save_dir+"/checkpoint.pth")
