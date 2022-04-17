c459656de4e4fc05f472d59084203aa58513eb63
Alan Cruz <cruz.alan@gmail.com>
2022-04-16 20:59:52 -0400

    first pass at cli arg parser

--- predict.py
+++ predict.py
@@ -33,46 +33,35 @@ import torchvision.transforms.functional as TF
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-s', action='store',
-                    dest='simple_value',
-                    help='Store a simple value')
+parser.add_argument('input',
+                    action='store',
+                    default="error.png",
+                    dest='input',
+                    required=True,
+                    help='Path to image')
 
-parser.add_argument('-c', action='store_const',
-                    dest='constant_value',
-                    const='value-to-store',
-                    help='Store a constant value')
+parser.add_argument('checkpoint',
+                    action='store',
+                    default="checkpoint.pth",
+                    dest='checkpoint',
+                    required=True,
+                    help='checkpoint file')
 
-parser.add_argument('-t', action='store_true',
+parser.add_argument('--category_names',
+                    action='store',
+                    default="cat_to_name.json",
+                    dest='category_names',
+                    help='Mapping of categories to real names')
+
+parser.add_argument('--gpu', 
+                    action='store',
                     default=False,
-                    dest='boolean_t',
-                    help='Set a switch to true')
-parser.add_argument('-f', action='store_false',
-                    default=True,
-                    dest='boolean_f',
-                    help='Set a switch to false')
+                    dest="gpu"
+                    help='Use GPU for inference')
 
-parser.add_argument('-a', action='append',
-                    dest='collection',
-                    default=[],
-                    help='Add repeated values to a list')
+cli_args = parser.parse_args()
 
-parser.add_argument('-A', action='append_const',
-                    dest='const_collection',
-                    const='value-1-to-append',
-                    default=[],
-                    help='Add different values to list')
-parser.add_argument('-B', action='append_const',
-                    dest='const_collection',
-                    const='value-2-to-append',
-                    help='Add different values to list')
-
-parser.add_argument('--version', action='version',
-                    version='%(prog)s 1.0')
-
-results = parser.parse_args()
-print('simple_value     = {!r}'.format(results.simple_value))
-print('constant_value   = {!r}'.format(results.constant_value))
-print('boolean_t        = {!r}'.format(results.boolean_t))
-print('boolean_f        = {!r}'.format(results.boolean_f))
-print('collection       = {!r}'.format(results.collection))
-print('const_collection = {!r}'.format(results.const_collection))
+print('input          = {!r}'.format(cli_args.input))
+print('checkpoint     = {!r}'.format(cli_args.checkpoint))
+print('category_names = {!r}'.format(cli_args.category_names))
+print('gpu            = {!r}'.format(cli_args.gpu))
--- train.py
+++ train.py
@@ -35,46 +35,48 @@ import torchvision.transforms.functional as TF
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-s', action='store',
-                    dest='simple_value',
-                    help='Store a simple value')
+parser.add_argument('data_dir',
+                    action='store',
+                    default="data_dir",
+                    dest='data_dir',
+                    required=True,
+                    help='Data directory')
 
-parser.add_argument('-c', action='store_const',
-                    dest='constant_value',
-                    const='value-to-store',
-                    help='Store a constant value')
+parser.add_argument('--save_dir',
+                    action='store',
+                    default="~/opt",
+                    dest='save_dir',
+                    help='Set directory to save checkpoints')
 
-parser.add_argument('-t', action='store_true',
-                    default=False,
-                    dest='boolean_t',
-                    help='Set a switch to true')
-parser.add_argument('-f', action='store_false',
-                    default=True,
-                    dest='boolean_f',
-                    help='Set a switch to false')
+parser.add_argument('--arch', 
+                    action='store',
+                    default="vgg13",
+                    dest="arch",
+                    help='Choose architecture')
 
-parser.add_argument('-a', action='append',
-                    dest='collection',
-                    default=[],
-                    help='Add repeated values to a list')
+parser.add_argument('--learning_rate', 
+                    action='store',
+                    default=0.01,
+                    dest='learning_rate',
+                    help='hyperparameter')
 
-parser.add_argument('-A', action='append_const',
-                    dest='const_collection',
-                    const='value-1-to-append',
-                    default=[],
-                    help='Add different values to list')
-parser.add_argument('-B', action='append_const',
-                    dest='const_collection',
-                    const='value-2-to-append',
-                    help='Add different values to list')
+parser.add_argument('--hidden_units', 
+                    action='store',
+                    default=512,
+                    dest='hidden_units',
+                    help='hyperparameter')
 
-parser.add_argument('--version', action='version',
-                    version='%(prog)s 1.0')
+parser.add_argument('--epochs', 
+                    action='store',
+                    default=1,
+                    dest='epochs',
+                    help='hyperparameter')
 
-results = parser.parse_args()
-print('simple_value     = {!r}'.format(results.simple_value))
-print('constant_value   = {!r}'.format(results.constant_value))
-print('boolean_t        = {!r}'.format(results.boolean_t))
-print('boolean_f        = {!r}'.format(results.boolean_f))
-print('collection       = {!r}'.format(results.collection))
-print('const_collection = {!r}'.format(results.const_collection))
+cli_args = parser.parse_args()
+
+print('data_dir      = {!r}'.format(results.data_dir))
+print('save_dir      = {!r}'.format(results.save_dir))
+print('arch          = {!r}'.format(results.arch))
+print('learning_rate = {!r}'.format(results.learning_rate))
+print('hidden_units  = {!r}'.format(results.hidden_units))
+print('epochs        = {!r}'.format(results.epochs))
